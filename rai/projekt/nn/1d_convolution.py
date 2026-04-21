9deb248fe86e1cc1b53b6790ff6d68a725ef5472
dinisius <246939@vutbr.cz>
2026-04-21 22:33:39 +0200

    Fix error, test "first try" nn for the first time (refs #4)

--- rai/projekt/nn/1d_convolution.py
+++ rai/projekt/nn/1d_convolution.py
@@ -1,3 +1,5 @@
+#TODO: WRITE COMMENTS TO EVERY CODE! 
+
 import pandas as pd
 import tensorflow as tf
 
@@ -41,8 +43,8 @@ def create_model(vocab_size, num_labels, vectorizer=None):
     my_layers = [vectorizer]
 
   my_layers.extend([
-      layers.Embedding(vocab_size, 64, mask_zero=True),
-      layers.Dropout(0.5),
+      layers.Embedding(vocab_size, 16, mask_zero=True),
+      layers.Dropout(0.7),
       layers.Conv1D(64, 5, padding="valid", activation="relu", strides=2),
       layers.GlobalMaxPooling1D(),
       layers.Dense(num_labels)
@@ -54,10 +56,24 @@ def create_model(vocab_size, num_labels, vectorizer=None):
 # `vocab_size` is `VOCAB_SIZE + 1` since `0` is used additionally for padding.
 int_model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=5, vectorizer=int_vectorize_layer)
 
-tf.keras.utils.plot_model(int_model, show_shapes=True)
+# tf.keras.utils.plot_model(int_model, show_shapes=True)
 
 int_model.compile(
     loss=losses.SparseCategoricalCrossentropy(from_logits=True),
     optimizer='adam',
     metrics=['accuracy'])
-int_history = int_model.fit(emotions_features[emotions_features.columns[0]], emotions_lables, batch_size=batch_size, epochs=10, validation_data=(test_features[test_features.columns[0]], test_lables))
+
+# 1. Vytvoření "hlídače", který zastaví trénink
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
+
+# 2. Tvůj upravený příkaz fit (přidal jsem parametr "callbacks" nakonec)
+int_history = int_model.fit(
+    emotions_features[emotions_features.columns[0]].to_numpy(), 
+    emotions_lables, 
+    batch_size=batch_size, 
+    epochs=50, # Teď můžeš s klidem nastavit hodně epoch
+    validation_data=(test_features[test_features.columns[0]].to_numpy(), test_lables), 
+    callbacks=[early_stop]
+)
+
+# int_history = int_model.fit(emotions_features[emotions_features.columns[0]].to_numpy(), emotions_lables, batch_size=batch_size, epochs=10, validation_data=(test_features[test_features.columns[0]].to_numpy(), test_lables))
