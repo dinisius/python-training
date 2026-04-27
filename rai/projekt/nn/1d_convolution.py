6a06aef76ed28f51b0a4e34e417ff268aff23104
dinisius <246939@vutbr.cz>
2026-04-27 07:36:30 +0200

    Add confusion matrix, edit comments (refs #4)

--- rai/projekt/nn/1d_convolution.py
+++ rai/projekt/nn/1d_convolution.py
@@ -2,66 +2,104 @@
 
 import pandas as pd
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.keras import utils
 from tensorflow.keras.layers import TextVectorization
 
-emotions_train = pd.read_csv("preprocessing/data_cleared_train.tsv", sep="\t")          # Read dataset
-emotions_test = pd.read_csv("preprocessing/data_cleared_test.tsv", sep="\t")          # Read dataset
+                                                    ###################
+                                                    # * READ DATASETS #
+                                                    ###################
+NUMBER_OF_LAYERS = 5                                                                    
 
-emotions_features = emotions_train.copy()
-emotions_features = emotions_features.drop(columns='Unnamed: 0')
-emotions_lables = emotions_features.pop('index')
+# Load the datasets
+emotions_train = pd.read_csv("preprocessing/data_cleared_train.tsv", sep="\t")
+emotions_test = pd.read_csv("preprocessing/data_cleared_test.tsv", sep="\t")            
 
-test_features = emotions_test.copy()
-test_features = test_features.drop(columns='Unnamed: 0')
-test_lables = test_features.pop('index')
+# Preprocess datasets
+emotions_features = emotions_train.copy()                                               # Copy train dataset to and then pop index column
+emotions_lables = emotions_features.pop('index')                                        # That's how i obtain features and labels
+emotions_features = emotions_features.drop(columns='Unnamed: 0')                        # Drop unusable first column
 
-batch_size = 32
 
-VOCAB_SIZE = 10000
-MAX_SEQUENCE_LENGTH = 50
+test_features = emotions_test.copy()                                                    # Copy test dataset to and then pop index column
+test_features = test_features.drop(columns='Unnamed: 0')                                # That's how i obtain features and labels
+test_lables = test_features.pop('index')                                                # Drop unusable first column
 
+                                                    #######################
+                                                    # * CREATE VOCABILARY #
+                                                    #######################
+
+# * Almost every step was copied from Tensor Flows "Load text" article: https://www.tensorflow.org/tutorials/load_data/text
+
+# TODO: Change following 3 variables and follow their influence on the nn
+batch_size = 32                                                                         
+VOCAB_SIZE = 10000                                                                      # Set maximum size of vocaulary (of unique words)
+MAX_SEQUENCE_LENGTH = 50                                                                # This parameter sets strict length of input tockens                  
+
+# Standarize, tokenize and vectorize within TextVectorization layer. For 1D conv nn 'int' mode is used.
+# * The default vectorization mode is 'int' (output_mode='int'). This outputs integer indices (one per token). \
+# * This mode can be used to build models that take word order into account.
 int_vectorize_layer = TextVectorization(
     max_tokens=VOCAB_SIZE,
     output_mode='int',
     output_sequence_length=MAX_SEQUENCE_LENGTH)
 
-int_vectorize_layer.adapt(emotions_features[emotions_features.columns[0]])
+# Create vocabulary based on train dataset
+int_vectorize_layer.adapt(emotions_features[emotions_features.columns[0]])              
 
+                                                    #######################
+                                                    # * "HAWKEYE" CONTROL #
+                                                    #######################
+
+# Show one sample of the vocabilary
 print("Emotional text in human format", emotions_features[emotions_features.columns[0]].loc[emotions_features.index[0]])
+print("Emotional label in human format", emotions_lables[0])
+print("Emotional text in 'int' vectorized format", \
+       int_vectorize_layer(emotions_features[emotions_features.columns[0]].loc[emotions_features.index[0]]).numpy())    # ? Looks like .numpy() is \
+                                                                                                                        # ? mandatory in this case 
 
-print("Emotional text in human format", emotions_lables[0])
+                                                    ########################
+                                                    # * SET NEURAL NETWORK #
+                                                    ########################
 
-print("Emotional text in 'int' vectorized format", int_vectorize_layer(emotions_features[emotions_features.columns[0]].loc[emotions_features.index[0]]).numpy())
+# * 1D Convolutional case (no freaking way, see the name of this source file)
+
+# TODO Useful articles:
+                        # * layers.Embedding: https://www.tensorflow.org/api_docs/python/tf/keras/layers/Embedding
+                        # * Conv1D: https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv1D
 
 def create_model(vocab_size, num_labels, vectorizer=None):
-  my_layers =[]
-  if vectorizer is not None:
-    my_layers = [vectorizer]
+    my_layers =[]
+    if vectorizer is not None:
+        my_layers = [vectorizer]
 
-  my_layers.extend([
-      layers.Embedding(vocab_size, 16, mask_zero=True),
-      layers.Dropout(0.7),
-      layers.Conv1D(64, 5, padding="valid", activation="relu", strides=2),
-      layers.GlobalMaxPooling1D(),
-      layers.Dense(num_labels)
-  ])
+    my_layers.extend([
+        layers.Embedding(vocab_size, output_dim=16, mask_zero=True),                       # TODO: Play with output_dim=16 parameter to compare results               
+        layers.Dropout(0.7),                                                                        # TODO: Play with Dropout parameter
+        layers.Conv1D(filters=64, kernel_size=5, strides=2, padding="valid", activation="relu"),    # TODO: Play with all the parameters (PAT strides)
+        layers.GlobalMaxPooling1D(),                                                        # TODO: return to this method for better understanding
+        layers.Dense(num_labels)                                                            # Just my regular densely-connected NN layer.
+    ])
 
-  model = tf.keras.Sequential(my_layers)
-  return model
+    model = tf.keras.Sequential(my_layers)
+    return model
 
-# `vocab_size` is `VOCAB_SIZE + 1` since `0` is used additionally for padding.
-int_model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=5, vectorizer=int_vectorize_layer)
-
-# tf.keras.utils.plot_model(int_model, show_shapes=True)
+# 'vocab_size' is 'VOCAB_SIZE + 1' since '0' is used additionally for padding.
+int_model = create_model(vocab_size=VOCAB_SIZE + 1, num_labels=NUMBER_OF_LAYERS, vectorizer=int_vectorize_layer)
 
 int_model.compile(
-    loss=losses.SparseCategoricalCrossentropy(from_logits=True),
+    loss=losses.SparseCategoricalCrossentropy(from_logits=True),                            # TODO: Find out, what is logit
     optimizer='adam',
     metrics=['accuracy'])
+
+                                                    ######################
+                                                    # * LEARNING PROCESS #
+                                                    ######################
 
 # 1. Vytvoření "hlídače", který zastaví trénink
 early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=2, restore_best_weights=True)
@@ -75,5 +113,36 @@ int_history = int_model.fit(
     validation_data=(test_features[test_features.columns[0]].to_numpy(), test_lables), 
     callbacks=[early_stop]
 )
+
+                                                    ######################
+                                                    # * CONFUSION MATRIX #
+                                                    ######################
+
+# 1. Necháme model tipnout emoce pro všechna testovací data
+# (Nezapomněli jsme na náš .to_numpy() trik)
+raw_predictions = int_model.predict(test_features[test_features.columns[0]].to_numpy())
+
+# 2. Model vrací tzv. logits (syrová čísla pro každou z 5 emocí). 
+# Potřebujeme vybrat tu emoci, která dostala nejvyšší skóre.
+predicted_labels = np.argmax(raw_predictions, axis=1)
+
+# 3. Spočítáme matici záměn (porovnáme skutečnost s tipy modelu)
+cm = confusion_matrix(test_lables, predicted_labels)
+
+# 4. Vykreslení matice
+# Tady si pak můžeš do display_labels dopsat reálné názvy svých emocí ve správném pořadí (0-4)
+emotions_names = ['Emoce 0', 'Emoce 1', 'Emoce 2', 'Emoce 3', 'Emoce 4'] 
+
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=emotions_names)
+disp.plot(cmap=plt.cm.Blues)
+
+# Zvětšíme obrázek, ať je to hezky čitelné
+fig = plt.gcf()
+fig.set_size_inches(8, 8)
+plt.title("Matice záměn (Confusion Matrix)")
+plt.show()
+
+
+# accuracy: 0.9113 - loss: 0.3019 - val_accuracy: 0.8687 - val_loss: 0.3816
 
 # int_history = int_model.fit(emotions_features[emotions_features.columns[0]].to_numpy(), emotions_lables, batch_size=batch_size, epochs=10, validation_data=(test_features[test_features.columns[0]].to_numpy(), test_lables))
--- rai/projekt/preprocessing/preprocessing_train.py
+++ rai/projekt/preprocessing/preprocessing_train.py
@@ -19,66 +19,77 @@ print(df.head())                                            # Print original dat
 
 rows, columns = df.shape                                    # Get the size of original dataset
 
-multivalue_index_list = []                                  # Init list of indexes with multi emotion text
+multi_emotion_index_list = []                               # Init empty list of indexes with multi emotion text
 
-my_nn_emotions = (2, 14, 15, 17, 25)                        # Init tupple with emotions(indexes) that will be used in my neural network. \
-                                                            # * 2 = Anger, 14 = Fear, 15 = Gratitude, 17 = Joy, 25 = Sadness
+# Init tupple with emotions(indexes) that will be used in my neural network
+my_nn_emotions = (2, 14, 15, 17, 25)                        # * 2 = Anger, 14 = Fear, 15 = Gratitude, 17 = Joy, 25 = Sadness                        
 
-not_my_nn_emotions = []                                     # Init empty list of indexes of text, that is out of my emotions scope
+not_my_nn_emotions = []                                     # Init empty list of indexes of emotions, that is out of my scope
 
 for index in range(rows):                                   #TODO: Try to implement enumerate() fashion loop here
 
-    stop = False
+    stop = False                                            # Support variable for braking for loop
 
-    if len(df[df.columns[1]].loc[df.index[index]]) >= 3:
+    # Detects multi emotion text
+    if len(df[df.columns[1]].loc[df.index[index]]) >= 3:    # Dataset has only 28 emotion classes. Max length of 1-emotion text = 2 
 
-            tmp = df[df.columns[1]].loc[df.index[index]]
+            # Copy value of the particular cell to tmp variable
+            tmp = df[df.columns[1]].loc[df.index[index]]    # Cell contains values like '8,20', its type is str                                                            
 
-            for multy in tmp.split(','):
+            for multy in tmp.split(','):                    # Iterate "subvalues"(emotions) of splitted with comma tmp value  
                  
-                 if int(multy) in my_nn_emotions:
-                      stop = True
-                      df.replace(df[df.columns[1]].loc[df.index[index]], multy, inplace = True)
-                      break
-            if stop == True:
-                continue
+                # If at least one "subvalue" is in my emotion scope, rewrite value with current "subvalue" and break the for loop.
+                # * So, I rewrite whole multi emotion value with the first detected "in my scope" emotion.
+                # * E.g., if original value was '2,14,25', cell would be rewritten with '2'.
+                if int(multy) in my_nn_emotions:            # Type cast for detection multy in my_nn_emotions
+                    stop = True  
+                    df.replace(df[df.columns[1]].loc[df.index[index]], multy, inplace = True)
+                    break
+                
+                if stop == True:
+                    continue
 
-            multivalue_index_list.append(index)                 # Add current index to the list.
-        
-        # print(df_cleared[df_cleared.columns[0]].loc[df_cleared.index[index]]) #Left it here because of .loc method
+            multi_emotion_index_list.append(index)          # Otherwise, add current index to the list.
 
-df_cleared = df.drop(index=multivalue_index_list)         
-df_cleared = df_cleared.drop(columns="reddit_name")                 # Drop reddit_name column and pass changed dataset to "cleared" copy 
+df_cleared = df.drop(index=multi_emotion_index_list)        # Drop rows with multi emotion vaules without my scope emotions 
+df_cleared = df_cleared.drop(columns="reddit_name")         # Drop reddit_name column due to unusability
 print(df_cleared.head())                                    # Print "cleared" dataset
-# df_cleared = df.drop(df.index[not_my_nn_emotions])          # Drop indexes with multi emotion at once. 
+
+
+                                #####################################################
+                                # * Now texts of dataset have only 1 assigned value #
+                                #####################################################
+
+
+# df_cleared = df.drop(df.index[not_my_nn_emotions])        # Drop indexes with multi emotion at once. 
                                                             # ? Which way of dropping is correct? 
 
-print("Raw rows:", rows, "Raw columns", columns)            # Print original dataset size \
-                                                            # * 43410 rows and 3 columns
+# Print original dataset size
+print("Raw rows:", rows, "Raw columns", columns)            # * 43410 rows and 3 columns
+                                                             
+cleared_rows, cleared_columns = df_cleared.shape            # Get "cleared" dataset size and print it
+print("Cleared rows:", cleared_rows, "Cleared columns", cleared_columns)    # * 36308 rows and 3 columns
 
-cleared_rows, cleared_columns = df_cleared.shape            # Get "cleared" dataset size
-
-print("Cleared rows:", cleared_rows, "Cleared columns", cleared_columns)    # Print "cleared" dataset size \
-                                                                            # * # * 36308 rows and 2 columns
-
-for index in range(cleared_rows):                           # Get indexes of text, that is out of my emotions scope
+# Get indexes of text, that is out of my emotions scope
+for index in range(cleared_rows):                           
         
-    if (int)(df_cleared[df_cleared.columns[1]].loc[df_cleared.index[index]]) not in my_nn_emotions:
+    # Type cast for detection index in my_nn_emotions
+    if (int)(df_cleared[df_cleared.columns[1]].loc[df_cleared.index[index]]) not in my_nn_emotions: 
 
-        not_my_nn_emotions.append(index)                    # Add index if index is out of my emotions scope
+        not_my_nn_emotions.append(index)                    # If index is out of my emotions scope add it to not_my_nn_emotions
 
-print("not_my_nn_emotions length: ", len(not_my_nn_emotions))   # Print how many text is out of my emotions scope \
-                                                                # * not_my_nn_emotions length:  31326
+# Print how many text is out of my emotions scope
+print("not_my_nn_emotions length: ", len(not_my_nn_emotions))   # * not_my_nn_emotions length:  31326
                                                                 
 df_tmp = copy.deepcopy(df_cleared)                              # ? Copy df_cleared in df_tmp for more ensurance
 
 df_cleared = df_tmp.drop(df_tmp.index[not_my_nn_emotions])      # Drop indexes of text with emotions that are out of scope at once. 
 
-cleared_rows, cleared_columns = df_cleared.shape            # Get "cleared" dataset size
-print("Cleared rows:", cleared_rows, "Cleared columns", cleared_columns)    # Print "cleared" dataset size  \
-                                                                            # * 4982  rows and 2 columns
+cleared_rows, cleared_columns = df_cleared.shape            # Get "cleared" dataset size and print it
+print("Cleared rows:", cleared_rows, "Cleared columns", cleared_columns)    # * 4982  rows and 2 columns
 
-df_cleared['index'] = df_cleared['index'].astype('category')                # Perform this for easy bar plotting of pd.Series type
+df_cleared['index'] = df_cleared['index'].astype('category')                # ? Perform this for easy bar plotting of pd.Series type
+
 df_cleared['index'] = df_cleared['index'].cat.rename_categories({           # Rename indexes to appropiate emotions for better readability
                                                                 '2':'Anger', 
                                                                 '14':'Fear', 
@@ -87,7 +98,25 @@ df_cleared['index'] = df_cleared['index'].cat.rename_categories({           # Re
                                                                 '25':'Sadness'
                                                                 })
 
-df_cleared['index'] = df_cleared['index'].cat.rename_categories({           # Rename indexes to appropiate emotions for better readability
+emotions_frequency = df_cleared['index'].value_counts()                     # Count frequency of each emotion text in preprocessed dataset
+
+# Print frequency of emotions
+print(emotions_frequency)                                   # * Gratitude: 2649
+                                                            # * Anger:     1567
+                                                            # * Joy:       1370
+                                                            # * Sadness:   1265
+                                                            # * Fear:       591
+
+# Plot the bar graf
+emotions_frequency.plot(kind='bar')                         
+plt.title('Emotions frequency in preprocessed dataset')
+plt.xlabel('Emotions')
+plt.ylabel('Frequency')
+plt.grid(True)                                                                            
+plt.show()
+
+# Rename indexes with their original name due to Tensor Flow requirements
+df_cleared['index'] = df_cleared['index'].cat.rename_categories({           
                                                                 'Anger':'0', 
                                                                 'Fear':'1', 
                                                                 'Gratitude':'2', 
@@ -95,22 +124,6 @@ df_cleared['index'] = df_cleared['index'].cat.rename_categories({           # Re
                                                                 'Sadness':'4'
                                                                 })
 
-emotions_frequency = df_cleared['index'].value_counts()                     # Count frequency of each emotion text in preprocessed dataset
+print(emotions_frequency)                                   # Print emotions frequency values
 
-print(emotions_frequency)                                   # Print frequency of emotions. -> after "smarter" preprocessing
-                                                            # * Gratitude: 1857 -> 2649
-                                                            # * Anger:     1025 -> 1567
-                                                            # * Joy:        853 -> 1370
-                                                            # * Sadness:    817 -> 1265
-                                                            # * Fear:       430 ->  591
-
-emotions_frequency.plot(kind='bar')                         # Plot the bar graf
-plt.title('Emotions frequency in preprocessed dataset')
-plt.xlabel('Emotions')
-plt.ylabel('Frequency')
-plt.grid(True)                                                                            
-plt.show()
-
-print(emotions_frequency)
-
-# df_cleared.to_csv('data_cleared.tsv', sep="\t")
+df_cleared.to_csv('data_cleared_train.tsv', sep="\t")        # Save cleared dataset
